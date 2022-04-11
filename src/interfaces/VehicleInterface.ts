e7f9ea152dc8be709fc871a6a450ad0644f43df2
Julio <julioaranabr@yahoo.com.br>
2022-04-11 15:38:17 -0300

    interface vehicle

---
+++ src/interfaces/VehicleInterface.ts
@@ -0,0 +1,14 @@
+import { z } from 'zod';
+
+const vehicleSchema = z.object({
+  model: z.string()
+    .min(3, { message: 'Model must be 3 or more characters long' }),
+  year: z.number().gte(1900, { message: 'Year must be bigger or equal 1900' })
+    .lte(2022, { message: 'Year must be less or equal 2022' }),
+  color: z.string()
+    .min(3, { message: 'Color must be 3 or more characters long' }),
+  status: z.boolean().optional(),
+  buyValue: z.number().int(),
+});
+
+export type Vehicle = z.infer<typeof vehicleSchema>;
