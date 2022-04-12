12e9a35d379b7784a2f2488ae7009d7c9ecf35e9
Julio <julioaranabr@yahoo.com.br>
2022-04-11 21:27:54 -0300

    refatored CarController

--- src/controllers/CarController.ts
+++ src/controllers/CarController.ts
@@ -4,9 +4,17 @@ import CarService from '../services/CarService';
 import { Car } from '../interfaces/CarInterface';
 
 class CarController extends Controller<Car> {
-  constructor(service = new CarService()) {
+  private _route: string;
+
+  constructor(
+    service = new CarService(),
+    route = '/cars',
+  ) {
     super(service);
+    this._route = route;
   }
+
+  get route() { return this._route; }
 
   create = async (
     req: RequestWithBody<Car>,
