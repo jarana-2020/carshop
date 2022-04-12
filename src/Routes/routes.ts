4427a6abb68d8b4f042fd0426d498a027488c833
Julio <julioaranabr@yahoo.com.br>
2022-04-11 21:26:17 -0300

    created Router.ts

--- src/Routes/routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import CarController from '../controllers/CarController';
-
-const carController = new CarController();
-const routes = Router();
-
-routes.post('/cars', carController.create);
-
-export default routes;
