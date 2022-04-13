3ceff55904db08bb136abaea689566f72a80cdfe
Julio <julioaranabr@yahoo.com.br>
2022-04-13 19:23:27 -0300

    test motorcycle service

--- src/tests/unit/services/motorcycle.test.ts
+++ src/tests/unit/services/motorcycle.test.ts
@@ -1,38 +1,136 @@
 import { expect } from 'chai';
 import Sinon from 'sinon';
-import { validMotorcycle } from '../../../../__tests__/utils/MotorcyclesMock';
 import { VehicleCar } from '../../../interfaces/CarInterface';
 import MotorcycleModel from '../../../models/Motorcycle';
 import MotorcycleService from '../../../services/MotorcycleService';
-import { errorsZod, mockResolvesCar, objCar } from '../../mocks/mocks';
+import { errorsZod, mockResolvesCar, mockResolvesMotorcycle, objCar, objMotorcycle } from '../../mocks/mocks';
 
-// describe('#Testa o método create', () => {
+describe('Car Service', () => {
+  let serviceMotorcycle = new MotorcycleService();
+  const mockModelMotorcycle = [mockResolvesMotorcycle];
 
-//   const motorcycleService = new MotorcycleService();
+  describe('#Testa o método create', () => {
+
+    before(() => {
+      Sinon.stub(serviceMotorcycle.model, 'create').resolves(mockResolvesMotorcycle);
+    });
+
+    after(() => {
+      Sinon.restore();
+    });
+
+    it('Deve retornar um objeto com suas devidas chaves', async () => {
+      const motorcycle = await serviceMotorcycle.create(objMotorcycle);
+      expect(motorcycle).to.be.an('object');
+      expect(motorcycle).to.contain.keys('_id', 'model', 'year', 'color', 'buyValue', 'category', 'engineCapacity');
+    });
+  })
+
+  describe('#Testa o metodo read', () => {
+
+    before(() => {
+      Sinon.stub(serviceMotorcycle.model, 'read').resolves(mockModelMotorcycle);
+    });
+
+    after(() => {
+      Sinon.restore();
+    });
+
+    it('Deve retornar um array de objetos', async () => {
+      const motorcycle = await serviceMotorcycle.read();
+      expect(motorcycle).to.be.an('array');
+      expect(motorcycle).to.have.length(1);
+      expect(motorcycle[0]).to.contain.keys('_id', 'model', 'year', 'color', 'buyValue', 'category', 'engineCapacity');
+    });
+  })
+
+  describe('#Testa o metodo readOne', () => {
+
+    before(() => {
+      Sinon.stub(serviceMotorcycle.model, 'readOne').resolves(mockResolvesMotorcycle);
+    });
+
+    after(() => {
+      Sinon.restore();
+    });
+
+    it('Deve retornar um objeto', async () => {
+      const motorcycle = await serviceMotorcycle.readOne(mockResolvesMotorcycle._id);
+      expect(motorcycle).to.be.an('object');
+      expect(motorcycle).to.contain.keys('_id', 'model', 'year', 'color', 'buyValue', 'category', 'engineCapacity');
+    });
+  })
+
+  describe('#Testa o metodo update', () => {
+
+    before(() => {
+      Sinon.stub(serviceMotorcycle.model, 'update').resolves(mockResolvesMotorcycle);
+    });
+
+    after(() => {
+      Sinon.restore();
+    });
+
+    it('Deve retornar um objeto', async () => {
+      const motorcycle = await serviceMotorcycle.update(mockResolvesMotorcycle._id, objMotorcycle);
+      expect(motorcycle).to.be.an('object');
+      expect(motorcycle).to.contain.keys('_id', 'model', 'year', 'color', 'buyValue', 'category', 'engineCapacity');
+    });
+  })
+
+  describe('#Testa o metodo delete', () => {
+
+    before(() => {
+      Sinon.stub(serviceMotorcycle.model, 'delete').resolves(mockResolvesMotorcycle);
+    });
+
+    after(() => {
+      Sinon.restore();
+    });
+
+    it('Deve retornar um objeto', async () => {
+      const motorcycle = await serviceMotorcycle.delete(mockResolvesMotorcycle._id);
+      expect(motorcycle).to.be.an('object');
+      expect(motorcycle).to.contain.keys('_id', 'model', 'year', 'color', 'buyValue', 'category', 'engineCapacity');
+    });
+  })
+})
+
+// describe('Service Car em caso de falha', () => {
+
+//   let serviceMotorcycle = new MotorcycleService();
 
 //   before(() => {
-//     Sinon.stub(motorcycleService.model, 'create').resolves({ 
-//     model: 'Honda CG Titan 125',
-//     year: 1963,
-//     color: 'red',
-//     buyValue: 3500,
-//     category: 'Street',
-//     engineCapacity: 125});
+//     Sinon.stub(VehicleCar, 'safeParse').returns(errorsZod as never);
 //   });
 
 //   after(() => {
 //     Sinon.restore();
 //   });
 
-//   it('Deve retornar um objeto com suas devidas chaves', async () => {
-//     const car = await motorcycleService.create({ 
-//     model: 'Honda CG Titan 125',
-//     year: 1963,
-//     color: 'red',
-//     buyValue: 3500,
-//     category: 'Street',
-//     engineCapacity: 125});
+//   it('retorna um objeto com uma chave erro', async () => {
+//     const car = await serviceMotorcycle.create(objCar);
 //     expect(car).to.be.an('object');
-//     expect(car).to.contain.keys('_id', 'model', 'year', 'color', 'buyValue', 'doorsQty', 'seatsQty');
+//     expect(car).contain.keys('error');
 //   });
+
+// })
+
+// describe('Testa o create do Service Genérico', () => {
+  
+//   const carModel = new CarModel();
+//   let serviceMotorcycle = new Service(carModel);
+
+//   before(() => {
+//     Sinon.stub(serviceMotorcycle.model,'create').resolves(objCar);
+//   });
+
+//   after(() => {
+//     Sinon.restore();
+//   });
+
+//   it('retorna um objeto com uma chave erro', async () => {
+//     const car = await serviceMotorcycle.create(objCar);
+//     expect(car).to.be.an('object');
+//   })
 // })
