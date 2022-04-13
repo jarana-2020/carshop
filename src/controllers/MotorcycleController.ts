7bf49ff6f04cd1864c95036accba7210fa7f69ac
Julio <julioaranabr@yahoo.com.br>
2022-04-13 19:34:26 -0300

    completed controller motorcycle

--- src/controllers/MotorcycleController.ts
+++ src/controllers/MotorcycleController.ts
@@ -1,4 +1,4 @@
-import { Response } from 'express';
+import { Response, Request } from 'express';
 import Controller, { RequestWithBody, ResponseError } from '.';
 import { Motorcycle } from '../interfaces/MotorcycleInterface';
 import MotorcycleService from '../services/MotorcycleService';
@@ -30,6 +30,56 @@ class MotorcycleController extends Controller<Motorcycle> {
         return res.status(400).json(motorcycle);
       }
       return res.status(201).json(motorcycle);
+    } catch (error) {
+      return res.status(500).json({ error: this.errors.internal });
+    }
+  };
+
+  readOne = async (
+    req: Request<{ id: string }>,
+    res: Response<Motorcycle | ResponseError>,
+  ): Promise<typeof res> => {
+    const { id } = req.params;
+    try {
+      const motorcycle = await this.service.readOne(id);
+      return motorcycle 
+        ? res.json(motorcycle)
+        : res.status(404).json({ error: this.errors.notFound });
+    } catch (error) {
+      return res.status(500).json({ error: this.errors.internal });
+    }
+  };
+
+  update = async (
+    req: Request<{ id: string }>,
+    res: Response<Motorcycle | ResponseError>,
+  ): Promise<typeof res> => {
+    const { body } = req;
+    const { id } = req.params;
+    try {
+      const motorcycle = await this.service.update(id, body);
+      if (!motorcycle) {
+        return res.status(404)
+          .json({ error: this.errors.notFound });
+      }
+      return res.status(200).json(motorcycle);
+    } catch (error) {
+      return res.status(500).json({ error: this.errors.internal });
+    }
+  };
+
+  delete = async (
+    req: Request<{ id: string }>,
+    res: Response<Motorcycle | ResponseError>,
+  ): Promise<typeof res> => {
+    const { id } = req.params;
+    try {
+      const motorcycle = await this.service.delete(id);
+      if (!motorcycle) {
+        return res.status(404)
+          .json({ error: this.errors.notFound });
+      }
+      return res.status(204).json();
     } catch (error) {
       return res.status(500).json({ error: this.errors.internal });
     }
